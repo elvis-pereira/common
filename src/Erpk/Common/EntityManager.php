afde2771aa86f5e948fff8d33515233ce58ed947
stil <przemekryciuk@gmail.com>
2013-06-21 00:59:43 +0200

    optional mode for read only database

--- src/Erpk/Common/EntityManager.php
+++ src/Erpk/Common/EntityManager.php
@@ -6,11 +6,24 @@ use Doctrine\ORM;
 class EntityManager
 {
     protected static $instance = null;
+    protected static $useCopy = false;
     
+    public static function useCopy($mode)
+    {
+        self::$useCopy = (bool)$mode;
+    }
+
     public static function getInstance()
     {
         if (!self::$instance) {
             $db = __DIR__.'/Storage.db';
+
+            if (self::$useCopy) {
+                $tempDir = sys_get_temp_dir();
+                $tempDb = $tempDir.'/erpk-common-6c814ae7-ceb3-4aeb-88f4-a42d8f05dcb6';
+                copy($db, $tempDb);
+                $db = $tempDb;
+            }
             
             $paths = array(__DIR__.'/Model');
             $conn = array(
