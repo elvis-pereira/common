4e2aad02f7b6afbf4305eb9439bfdd206dc23da8
stil <przemekryciuk@gmail.com>
2013-08-21 14:54:08 +0200

    Citizen helpers improvements
    - Added method's comments
    - Updated images' URLs
    - Added constants for different avatar sizes
    - Added unit tests for getAvatar()

--- src/Erpk/Common/Citizen/Helpers.php
+++ src/Erpk/Common/Citizen/Helpers.php
@@ -5,20 +5,45 @@ use DateTime;
 
 class Helpers
 {
+    const AVATAR_SMALL = 50;
+    const AVATAR_MEDIUM = 100;
+    const AVATAR_PROFILE = 142;
+    const AVATAR_ORIGINAL = null;
+
+    /**
+     * Returns citizen's avatar URL
+     * @param  int        $id         Citizen ID
+     * @param  DateTime   $createdAt  Date when citizen was created
+     * @param  int|null   $size       Size of avatar image
+     * @return string                 Citizen's avatar URL
+     */
     public static function getAvatar($id, DateTime $createdAt, $size = null)
     {
         return
-            'http://static.erepublik.com/uploads/avatars/Citizens/'.
+            'http://static.erepublik.net/uploads/avatars/Citizens/'.
             $createdAt->format('Y/m/d').'/'.
             md5($id).($size ? '_'.$size.'x'.$size : '').'.jpg';
     }
     
+    /**
+     * Returns influence made with single hit
+     * @param  int  $strength        Citizen's strength
+     * @param  int  $rankLevel       Citizen's rank level
+     * @param  integer $firePower    Weapon fire power
+     * @param  boolean $eliteCitizen Boolean if citizen is "elite"
+     * @return int                   Influence made with single hit
+     */
     public static function getHit($strength, $rankLevel, $firePower = 0, $eliteCitizen = false)
     {
         $mod = $eliteCitizen ? 1.1 : 1;
         return floor($mod*(10 * (1 + $strength/400) * (1 + $rankLevel/5) * (1 + $firePower/100)));
     }
     
+    /**
+     * Returns citizen's division
+     * @param  int $level Citizen's level
+     * @return int        Citizen's division
+     */
     public static function getDivision($level)
     {
         if ($level <= 24) {
--- tests/Erpk/Common/Tests/Citizen/HelpersTest.php
+++ tests/Erpk/Common/Tests/Citizen/HelpersTest.php
@@ -3,9 +3,35 @@ namespace Erpk\Common\Tests\Citizen;
 
 use Erpk\Common\Tests\TestCase;
 use Erpk\Common\Citizen\Helpers;
+use Erpk\Common\DateTime;
 
 class HelpersTest extends TestCase
 {
+    public function testGetAvatar()
+    {
+        $platoAvatar =
+            'http://static.erepublik.net/uploads/avatars/'.
+            'Citizens/2007/06/04/c81e728d9d4c2f636f067f89cc14862c';
+
+        $this->assertEquals(
+            $platoAvatar.'_142x142.jpg',
+            Helpers::getAvatar(
+                2,
+                new DateTime('2007-06-04'),
+                Helpers::AVATAR_PROFILE
+            )
+        );
+
+        $this->assertEquals(
+            $platoAvatar.'.jpg',
+            Helpers::getAvatar(
+                2,
+                new DateTime('2007-06-04'),
+                Helpers::AVATAR_ORIGINAL
+            )
+        );
+    }
+
     public function testGetHit()
     {
         $this->assertEquals(10590, Helpers::getHit(31212, 62));
