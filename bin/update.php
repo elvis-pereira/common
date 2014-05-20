97e5f0fb90d6f9a867df5da166ecf74eb2761afe
stil <przemekryciuk@gmail.com>
2014-05-20 15:17:34 +0200

    Cleanup

--- bin/update.php
+++ bin/update.php
@@ -4,10 +4,9 @@ if (count($argv) !== 3) {
     die("\nusage: php ".basename(__FILE__)." <private api key> <public api key>\n\n");
 }
 
-$client = new Erpk\Api\Client;
+$client = new Erpk\Api\Client();
 $client->setPrivateKey($argv[1]);
 $client->setPublicKey($argv[2]);
-
 /**
  * Load countries
  */
@@ -19,7 +18,7 @@ $countriesData = $countriesData['countries']['country'];
 
 $countriesIDs = array();
 foreach ($countriesData as $countryData) {
-    $country = new Erpk\Common\Entity\Country;
+    $country = new Erpk\Common\Entity\Country();
     $country->setId($countryData['id']);
     $country->setName($countryData['name']);
     $country->setCode($countryData['initials']);
@@ -36,7 +35,7 @@ foreach ($countriesIDs as $countryID) {
     $regionsData = $regionsData['regions']['region'];
     
     foreach ($regionsData as $regionData) {
-        $region = new Erpk\Common\Entity\Region;
+        $region = new Erpk\Common\Entity\Region();
         $region->setId($regionData['id']);
         $region->setName($regionData['name']);
         $region->setPermalink($regionData['permalink']);
